98a8f19df6133cba8f848a77752948cbc50c449f
Ronicley Silva <ronicleysantossilva@gmail.com>
2020-04-08 10:23:06 -0300

    deploy heroku

--- backend/settings.py
+++ backend/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
